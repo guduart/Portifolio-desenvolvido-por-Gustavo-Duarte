874ed0c9edf8240e1e82f01d772c9c716af07999
Gustavo Duarte <126031664+guduart@users.noreply.github.com>
2023-05-24 22:02:40 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# Portifolio-desenvolvido-por-Gustavo-Duarte
-Portfólio onde coloco um pouco daquilo que estudo e desenvolvo. Para o desenvolvimento deste site utilizei as seguintes tecnologias: HTML, CSS E JAVASCRIPT
+<h1 align="center">
+📰 Portfólio.
+</h1>
+
+<h4 align="center">
+  Portfólio onde coloco um pouco daquilo que estudo e desenvolvo.
+</h4>
+
+## 📚 Seções
+
+O site é composto por cinco seções:
+
+- **Home:** Nele temos uma breve apresentação;
+- **Sobre:** Nessa seção tenho uma descrição dizendo um pouco sobre quem sou;
+- **Habilidades:** Exibe através de cards as diferentes tecnologias em que possuo conhecimento;
+- **Projetos:** Apresenta alguns projetos desenvolvidos e com link direto para os respectivos códigos no GitHub;
+- **Contato:** Nessa seção são apresentadas as maneiras de entrar em contato comigo;
+
+## 🛠 Tecnologias utilizadas
+Para o desenvolvimento deste site utilizei as seguintes tecnologias:
+- Visual Studio Code (Editor de código);
+- HTML5;
+- CSS3;
+- JavaScript;
+---
+
---
+++ js/script.js
@@ -0,0 +1,125 @@
+/*--=============== RESPONSIVE MENU ===============--*/
+
+let nav = document.getElementById('navMenu');
+const menuIcon = document.getElementById('mobile-menu');
+
+function toogleMenu() {
+  nav.classList.toggle('openMobile');
+  menuIcon.classList.toggle('active');
+}
+
+function closeMenu() {
+  nav.classList.remove('openMobile');
+  menuIcon.classList.remove('active');
+}
+
+/*--=============== STICKY NAVBAR ===============--*/
+window.addEventListener('scroll', function () {
+  var navbar = document.querySelector('#navbar');
+  let scrollTop = document.querySelector('.ScrollTop');
+
+  scrollTop.classList.toggle('active', this.window.scrollY > 500);
+  navbar.classList.toggle('stick', window.scrollY > 0);
+});
+
+/*--=============== FORM ===============--*/
+
+var form = document.getElementById('my-form');
+
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById('status');
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+      Accept: 'application/json',
+    },
+  })
+    .then((response) => {
+      status.classList.add('success');
+      status.innerHTML =
+        'Sua mensagem foi enviada com sucesso! Entrarei em contato em breve.';
+      form.reset();
+    })
+    .catch((error) => {
+      status.classList.add('error');
+      status.innerHTML = 'Ocorreu um erro durante o envio da mensagem :(';
+    });
+}
+form.addEventListener('submit', handleSubmit);
+
+/*--=============== LINK ACTIVE ON SCROLL ===============--*/
+
+const links = document.querySelectorAll('.links');
+const sections = document.querySelectorAll('section');
+
+function changeLinkState() {
+  let index = sections.length;
+
+  while (--index && window.scrollY + 100 < sections[index].offsetTop) {}
+
+  links.forEach((link) => link.classList.remove('active'));
+  links[index].classList.add('active');
+}
+
+changeLinkState();
+window.addEventListener('scroll', changeLinkState);
+
+/*--=============== BUTTON LOAD MORE ===============--*/
+
+let loadMoreBtn = document.getElementById('load-more');
+let LoadMorebtnWrapper = document.getElementById('btnWrapper');
+let currentItem = 3;
+
+loadMoreBtn.addEventListener('click', () => {
+  let rows = [...document.querySelectorAll('#projects .row')];
+
+  for (let i = currentItem; i < currentItem + 1; i++) {
+    rows[i].style.display = 'flex';
+  }
+
+  currentItem += 1;
+
+  if (currentItem >= rows.length) {
+    LoadMorebtnWrapper.style.display = 'none';
+    LoadMorebtnWrapper.style.opacity = '0';
+    LoadMorebtnWrapper.style.visibility = 'hidden';
+    LoadMorebtnWrapper.style.pointerEvents = 'none';
+  }
+});
+
+/*--=============== GET CURRENT YEAR ===============--*/
+
+const YearDisplay = document.getElementById('CurrentYear');
+
+const currentYear = new Date();
+
+YearDisplay.innerText = currentYear.getFullYear();
+
+/*--=============== CHANGE NAVBAR IMG ON SCREEN SIZE ===============--*/
+
+const Logo = '../assets/img/logo.png';
+const whiteLogo = '../assets/img/logoWhite.png';
+const navbar = document.getElementById('navbar');
+const logo = document.querySelector('.logo');
+
+window.addEventListener('scroll', function () {
+  if (document.querySelector('.navbar.stick')) {
+    logo.firstChild.src = Logo;
+  } else {
+    logo.firstChild.src = whiteLogo;
+  }
+});
+
+/*--=============== FIXING NAVIGATIONS COVERING CONTENT ON SCROLL ===============--*/
+
+const navgationHeight = Math.floor(
+  document.querySelector('#navbar').offsetHeight,
+);
+
+document.documentElement.style.setProperty(
+  '--scroll-padding',
+  navgationHeight + 'px',
+);
